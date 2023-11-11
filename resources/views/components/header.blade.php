9f17a3a06f2f76158737e2e8d8157d5655acc97d
DazUki <absolute.dessan@gmail.com>
2023-11-11 20:23:46 +0100

    logo added

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,13 +3,14 @@
     <div class="container mx-auto flex justify-between items-center p-2 pl-2 max-w-6xl">
       <div class="flex justify-center text-gray-900 font-bold text-xl sm:text-2xl lg:text-3xl">
         <a href="/" class="antialiased items-center">
-          <img src="{{ asset('static/images/vinyl-record-svgrepo-com.svg') }}"
-            class="inline-block -mt-1 h-6 w-6 lg:w-8 lg:h-8">
+          {{-- <img src="{{ asset('static/images/vinyl-record-svgrepo-com.svg') }}"
+            class="inline-block -mt-1 h-6 w-6 lg:w-8 lg:h-8"> --}}
           <div class="inline-block items-center">
-            Vinylskivor Förteckning
+            <img src="{{ asset('static/images/vinyl-laravel_logo.png') }}"
+              class="h-[22px] lg:h-[49px] md:h-[32px]">
           </div>
           @auth
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block -mt-1 w-5 h-5 text-green-700">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block items-center -mt-2 lg:-mt-6 md:-mt-4 w-4 lg:w-8 md:w-6 text-green-700">
             <path fill-rule="evenodd" d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z" clip-rule="evenodd" />
           </svg>
           @endauth
