122d1cb8f478da8c93d09325a4f652dfa1bae7c3
Lars Lindberg <absolute.dessan@gmail.com>
2024-08-02 21:28:38 +0000

    vite.js fixes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
+import fs from 'fs';
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        server: {
+            host: true,
+            port: process.env.VITE_ASSET_PORT,
+            strictPort: true,
+            hmr: {
+                host: env.VITE_ASSET_HOST,
+                port: env.VITE_HMR_ASSET_PORT,
+            },
+            https: {
+                key: fs.readFileSync(env.VITE_PRIVKEY_PATH),
+                cert: fs.readFileSync(env.VITE_CERT_PATH),
+            },
+        },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
-        }),
-    ],
+        })
+    ]
+}
 });
