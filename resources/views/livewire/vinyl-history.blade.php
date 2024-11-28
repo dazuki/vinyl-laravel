5c8cd122043d372c29258ede56e8cfdba0f6ec2f
Lars Lindberg <absolute.dessan@gmail.com>
2024-11-28 12:09:09 +0100

    update node dependencies & auth for statistics in vinyl-history

--- resources/views/livewire/vinyl-history.blade.php
+++ resources/views/livewire/vinyl-history.blade.php
@@ -1,4 +1,4 @@
-@section('page-title')
+@section("page-title")
     Historik
 @endsection
 <div wire:init="init" class="max-w-screen-xl mx-auto mt-4 text-left">
@@ -18,39 +18,41 @@
             </div>
         </div>
         @if ($loadData == true)
-            <div wire:transition.opacity>
-                <div class="flex flex-col items-center justify-center">
-                    <p
-                        class="flex justify-between w-full px-2 py-1 text-xs text-center text-red-800 border-0 lg:text-xs lg:w-1/2">
-                        <span>Vinyler Med Okänt Datum:</span><span class="font-semibold">{{ $vinyler_old }}</span>
-                    </p>
-                    <p
-                        class="flex justify-between w-full px-2 py-1 text-xs text-center border-t-2 border-b-0 border-slate-300 text-emerald-800 bg-slate-200 lg:text-sm lg:w-1/2 sm:border-r-2 sm:border-l-2">
-                        <span class="text-gray-800"><i>(Genomsnitt)</i> Införskaffade Vinyler Per År:</span><span
-                            class="font-semibold">{{ $vinyler_avg_year }}</span>
-                    </p>
-                    <p
-                        class="flex justify-between w-full px-2 py-1 text-xs text-center border-t-0 border-b-2 border-slate-300 text-emerald-800 bg-slate-200 lg:text-sm lg:w-1/2 sm:border-r-2 sm:border-l-2">
-                        <span class="text-gray-800"><i>(Genomsnitt)</i> Införskaffade Vinyler Per Månad:</span><span
-                            class="font-semibold">{{ $vinyler_avg_month }}</span>
-                    </p>
-                </div>
+            <div>
+                @auth
+                    <div class="flex flex-col items-center justify-center">
+                        <p
+                            class="flex justify-between w-full px-2 py-1 text-xs text-center text-red-800 border-0 lg:text-xs lg:w-1/2">
+                            <span>Vinyler Med Okänt Datum:</span><span class="font-semibold">{{ $vinyler_old }}</span>
+                        </p>
+                        <p
+                            class="flex justify-between w-full px-2 py-1 text-xs text-center text-emerald-800 lg:text-sm lg:w-1/2">
+                            <span class="text-gray-800"><i>(Genomsnitt)</i> Införskaffade Vinyler Per År:</span><span
+                                class="font-semibold">{{ $vinyler_avg_year }}</span>
+                        </p>
+                        <p
+                            class="flex justify-between w-full px-2 py-1 text-xs text-center text-emerald-800 lg:text-sm lg:w-1/2">
+                            <span class="text-gray-800"><i>(Genomsnitt)</i> Införskaffade Vinyler Per Månad:</span><span
+                                class="font-semibold">{{ $vinyler_avg_month }}</span>
+                        </p>
+                    </div>
+                @endauth
                 @php
-                    $setDate = date('Y-m-d');
+                    $setDate = date("Y-m-d");
                     $sameDay = 1;
                 @endphp
                 @foreach ($vinyler as $vinyl)
-                    @if ($setDate != date('Y-m-d', strtotime($vinyl->created_at)) || $sameDay == 1)
+                    @if ($setDate != date("Y-m-d", strtotime($vinyl->created_at)) || $sameDay == 1)
                         <div class="flex items-center justify-center">
                             <div
-                                class="{{ $loop->first ? 'mt-2 mb-4 ' : 'my-4 ' }}w-full py-1 text-lg text-center border-b-2 border-t-2 bg-slate-100 border-slate-300 lg:text-xl lg:py-2 lg:w-1/2 sm:border-r-2 sm:border-l-2">
-                                <p class="font-semibold">{{ date('j/n', strtotime($vinyl->created_at)) }}</p>
+                                class="{{ $loop->first ? "mt-2 mb-4 " : "my-4 " }}w-full py-1 text-lg text-center border-b-2 border-t-2 bg-slate-100 border-slate-300 lg:text-xl lg:py-2 lg:w-1/2 sm:border-r-2 sm:border-l-2">
+                                <p class="font-semibold">{{ date("j/n", strtotime($vinyl->created_at)) }}</p>
                                 <p class="text-xs font-semibold text-gray-500 lg:text-sm">
-                                    {{ date('Y', strtotime($vinyl->created_at)) }}</p>
+                                    {{ date("Y", strtotime($vinyl->created_at)) }}</p>
                             </div>
                         </div>
                         @php
-                            $setDate = date('Y-m-d', strtotime($vinyl->created_at));
+                            $setDate = date("Y-m-d", strtotime($vinyl->created_at));
                             $sameDay = 0;
                         @endphp
                     @endif
