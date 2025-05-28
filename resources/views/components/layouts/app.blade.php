c4b927acd049b8b31e0540456be7b145a40e207d
Lars Lindberg <absolute.dessan@gmail.com>
2025-05-28 17:03:19 +0200

    font import change

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,10 +10,6 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset("static/images/favicon-32x32.png") }}">
 	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset("static/images/favicon-16x16.png") }}">
 	<link rel="manifest" href="{{ asset("static/images/site.webmanifest") }}">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&&family=RocknRoll+One&display=swap"
-		rel="stylesheet">
 
 	<title>@yield("page-title") - Vinylskivor Förteckning</title>
 	@vite(["resources/css/app.css", "resources/js/app.js"])
