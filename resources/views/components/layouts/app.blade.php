8510748521233f4d6389c95623e387bde70c2dd5
Lars Lindberg <absolute.dessan@gmail.com>
2025-05-28 16:53:58 +0200

    SEO optimizing

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Tonys Vinyl Förteckning">
 
 	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset("static/images/apple-touch-icon.png") }}">
 	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset("static/images/favicon-32x32.png") }}">
