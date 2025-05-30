538ad4601f938c69c44c813af1fca05429012ff2
Lars Lindberg <absolute.dessan@gmail.com>
2025-05-31 01:04:25 +0200

    rm vite-plugin-purgecss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        Vite::prefetch(concurrency: 3);
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import htmlPurge from "vite-plugin-purgecss";
 
 export default defineConfig({
   plugins: [
@@ -8,16 +7,5 @@ export default defineConfig({
       input: ["resources/css/app.css", "resources/js/app.js"],
       refresh: true,
     }),
-    htmlPurge({
-      content: [
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.js",
-        "./resources/js/**/*.css",
-      ],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
   ],
-  build: {
-    sourcemap: true,
-  },
 });
