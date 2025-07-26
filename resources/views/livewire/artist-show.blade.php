710f37eb6415b89c71952e459e6b41c2d046135e
Lars Lindberg <absolute.dessan@gmail.com>
2025-07-26 23:03:43 +0200

    New vinyl button reposition

--- resources/views/livewire/artist-show.blade.php
+++ resources/views/livewire/artist-show.blade.php
@@ -7,35 +7,8 @@
 		@endauth
 		<div x-data="{ show: false }">
 			<h1 class="artist-text-shadow rock-font text-center text-2xl font-bold text-gray-700 sm:text-4xl">
-				{{ trim($artist["name"]) }}</h1>
-			@auth
-				<p class="w-full py-2 text-center">
-					<button x-on:click="show = ! show"
-						id="btnEdit"
-						class="rounded-lg border-2 border-slate-300 bg-slate-100 px-2 py-1 hover:bg-slate-300">
-						<svg xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="h-6 w-6">
-							<path
-								d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
-						</svg>
-					</button>
-					<button id="btnDel"
-						onclick="confirm('Vill du ta bort artist {{ $artist["name"] }} och ALLA Vinyler till denna artist? (ALLA Vinyler kommer också försvinna från databasen)') || event.stopImmediatePropagation()"
-						wire:click="delete({{ $artist["id"] }})"
-						class="rounded-lg border-2 border-red-300 bg-red-100 px-2 py-1 hover:bg-red-300">
-						<svg xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="h-6 w-6">
-							<path fill-rule="evenodd"
-								d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-								clip-rule="evenodd" />
-						</svg>
-					</button>
-				</p>
-			@endauth
+				{{ trim($artist["name"]) }}
+			</h1>
 			@if ($artist->discogs_image_url)
 				<div class="mt-4 flex w-full justify-center">
 					<img src="{{ $artist->discogs_image_url }}"
@@ -44,20 +17,49 @@
 				</div>
 			@endif
 			@auth
-				<p class="mx-auto mb-2 mt-4 w-full text-center">
-					<a class="w-full rounded-lg border-2 border-green-600 bg-slate-100 p-2 text-gray-900 shadow-md hover:bg-green-100"
-						href="/create/vinyl?artist_id={{ $artist["id"] }}"
-						wire:navigate><svg xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="-mt-1 mr-1 inline-block h-6 w-6 text-green-700">
-							<path stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-						</svg>Ny Vinyl</a>
-				</p>
+				<div>
+					<p class="mx-auto mb-2 mt-4 w-full text-center">
+						<a class="rounded-lg border-2 border-green-800 bg-slate-100 px-4 py-2 text-lg hover:bg-slate-300"
+							href="/create/vinyl?artist_id={{ $artist["id"] }}"
+							wire:navigate>
+							<svg xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="-mt-1 mr-1 inline-block h-8 w-8 text-green-700">
+								<path stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+							</svg>Ny Vinyl</a>
+					</p>
+					<p class="w-full py-2 text-center">
+						<button x-on:click="show = ! show"
+							id="btnEdit"
+							class="rounded-lg border-2 border-slate-300 bg-slate-100 px-2 py-1 hover:bg-slate-300">
+							<svg xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="h-6 w-6">
+								<path
+									d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+							</svg>
+						</button>
+						<button id="btnDel"
+							onclick="confirm('Vill du ta bort artist {{ $artist["name"] }} och ALLA Vinyler till denna artist? (ALLA Vinyler kommer också försvinna från databasen)') || event.stopImmediatePropagation()"
+							wire:click="delete({{ $artist["id"] }})"
+							class="rounded-lg border-2 border-red-300 bg-red-100 px-2 py-1 hover:bg-red-300">
+							<svg xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="h-6 w-6">
+								<path fill-rule="evenodd"
+									d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+									clip-rule="evenodd" />
+							</svg>
+						</button>
+					</p>
+				</div>
 			@endauth
 			<p class="flex items-center justify-center">
 				@php
